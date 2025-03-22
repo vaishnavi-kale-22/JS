7162d81ec25f474c0c45f877cd70d072db620e17
vaishnavi-kale-22 <vaishnavikale2215@gmail.com>
2025-03-22 22:32:18 +0530

    first commit

---
+++ ArrowFunction.js
@@ -0,0 +1,17 @@
+// Normal function
+function greet() {
+    console.log("Welcome to the coding world!");
+}
+
+// Arrow function
+const greetUser = () => {
+    console.log("Hello, User!");
+}
+
+// Arrow function with parameters
+const multiply = (x, y) => x * y;
+
+console.log(multiply(5, 3)); // Output: 15
+
+greet();
+greetUser();
